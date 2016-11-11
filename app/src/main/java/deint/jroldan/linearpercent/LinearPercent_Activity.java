39b3080aa8b53379e33b86cb1bda261e7e367841
Jesús Roldán López <jroldan686@gmail.com>
2016-11-11 11:15:00 +0100

    Initial commit

---
+++ app/src/main/java/deint/jroldan/linearpercent/LinearPercent_Activity.java
@@ -0,0 +1,16 @@
+package deint.jroldan.linearpercent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LinearPercent_Activity extends AppCompatActivity {
+
+    Button btnA, btnB, btnC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_percent_);
+    }
+}
